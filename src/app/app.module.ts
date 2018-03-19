30a02084a7239d9ef407b96d31d1788b46166bb4
arkadiusz <skorek123141@gmail.com>
2018-03-19 21:49:55 +0100

    new version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AlertComponent } from './directives/alert/alert.component';
+import { RegisterComponent } from './register/register.component';
+import {AuthGuard} from './guards/auth.guard';
+import {AuthenticationService} from './service/authentication.service';
+import {UserService} from './service/user.service';
+import {FormsModule} from '@angular/forms';
+import { routing } from './app.routing';
+import {AlertService} from './service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,   
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
